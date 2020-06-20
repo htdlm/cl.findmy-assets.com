564404d81b4485658527e1d4d25ff735d43682e1
Alfred Ortega <omar.alfredo49@gmail.com>
2020-06-20 11:40:10 -0500

    Backoffice #1

--- backoffice/index.php
+++ backoffice/index.php
@@ -16,8 +16,6 @@ require_once "controladores/clientes.controlador.php";
 require_once "controladores/usuarios.controlador.php";
 require_once "controladores/clientes.controlador.php";
 
-
-
 require_once "modelos/usuarios.modelo.php";
 require_once "modelos/ventas.modelo.php";
 require_once "modelos/productos.modelo.php";
