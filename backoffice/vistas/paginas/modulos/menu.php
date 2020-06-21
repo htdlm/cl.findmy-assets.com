25c56894c52d237d84e0b449a34bacf200cf30aa
Alfred Ortega <omar.alfredo49@gmail.com>
2020-06-21 02:00:49 -0500

    Change the menu icon

--- backoffice/vistas/paginas/modulos/menu.php
+++ backoffice/vistas/paginas/modulos/menu.php
@@ -1,7 +1,7 @@
 <aside class="main-sidebar sidebar-dark-primary elevation-4">
   <!-- Brand Logo -->
   <a href="inicio" class="brand-link">
-  <img src="vistas/img/plantilla/logo.ico" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
+  <img src="vistas/img/vectors/Favicon.svg" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
     <span class="brand-text font-weight-light"> <?php echo DICPY ?> </span>
   </a>
 
@@ -10,7 +10,7 @@
     <!-- Sidebar user panel (optional) -->
     <div class="user-panel mt-3 pb-3 mb-3 d-flex">
       <div class="image">
-     
+
       <?php if ($usuario["foto"] == ""): ?>
 
          <img src="vistas/img/usuarios/default/default.png" class="img-circle elevation-2" alt="User Image">
@@ -18,7 +18,7 @@
       <?php else: ?>
 
         <img src="<?php echo $usuario["foto"] ?>" class="img-circle elevation-2" alt="User Image">
-        
+
       <?php endif ?>
 
       </div>
@@ -56,7 +56,7 @@
               <p>Usuarios</p>
             </a>
           </li>
-          
+
         <?php endif ?>
 
 
@@ -66,7 +66,7 @@
                   <p>Clientes/Empresas</p>
               </a>
           </li>
-      
+
 
         <li class="nav-item has-treeview">
           <a href="#" class="nav-link">
